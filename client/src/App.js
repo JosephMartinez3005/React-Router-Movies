698011df67ccf0e67b457afdd6d2c5d52b327c8f
JosephMartinez3005 <52721764+JosephMartinez3005@users.noreply.github.com>
2021-04-09 12:16:37 -0700

    Got the Movie List to Homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import {Route, Switch} from 'react-router-dom'
 import SavedList from './Movies/SavedList';
+import MovieList from './Movies/MovieList';
+
 
 export default function App () {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
@@ -9,11 +11,11 @@ export default function App () {
 
   useEffect(() => {
     const getMovies = () => {
-      axios
-        .get('http://localhost:5000/api/movies') // Study this endpoint with Postman
+      axios.get('http://localhost:5000/api/movies') // Study this endpoint with Postman
         .then(response => {
           // Study this response with a breakpoint or log statements
           // and set the response data as the 'movieList' slice of state
+          setMovieList(response.data)
         })
         .catch(error => {
           console.error('Server Error', error);
@@ -25,12 +27,19 @@ export default function App () {
   const addToSavedList = id => {
     // This is stretch. Prevent the same movie from being "saved" more than once
   };
-
+  console.log('Movie List', movieList)
   return (
     <div>
       <SavedList list={[ /* This is stretch */]} />
-
-      <div>Replace this Div with your Routes</div>
+      <div>
+        <Switch>
+      <Route 
+      exact
+      path='/'>
+        <MovieList movies={movieList}/>
+      </Route>
+      </Switch>
+      </div> 
     </div>
   );
 }
